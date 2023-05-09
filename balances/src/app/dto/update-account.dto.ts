73b4e52eaf910b59a6ac374910d454852801432a
Uriel Guy <urielsateles@gmail.com>
2023-05-09 10:55:40 -0300

    remove balances

--- balances/src/app/dto/update-account.dto.ts
+++
@@ -1,4 +0,0 @@
-export type UpdateAccountInput = {
-  id: string;
-  balance: number;
-};
--- balances/src/domain/repository/account.repository.ts
+++
@@ -1,8 +0,0 @@
-import { Account } from '../entity/account.entity';
-
-export interface AccountRepository {
-  findById(id: string): Promise<Account>;
-  update(account: Account): Promise<void>;
-  create(account: Account): Promise<void>;
-  delete(id: string): Promise<void>;
-}
